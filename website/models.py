463da5704b0d2985357fd8d9700a5af7a3eb7656
hussainisyed2009-ship-it <hussainisyed2009@gmail.com>
2026-07-29 19:54:48 -0400

    adjusting tables

--- website/models.py
+++ website/models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from sqlalchemy import CheckConstraint
 
 
 class User(db.Model, UserMixin):
@@ -8,9 +9,14 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    is_admin = db.Column(db.Boolean, default=False)
+    #is_admin = db.Column(db.Boolean, default=False)
     log_readings = db.relationship('Log_reading', backref='user', lazy=True)
-    teacher_class = db.relationship('class_list', backref='teacher', lazy='joined')
+    #teacher_class = db.relationship('class_list', backref='teacher', lazy='joined')
+        # Streak tracking fields
+    current_streak = db.Column(db.Integer, default=0)
+    last_activity_date = db.Column(db.Date, nullable=True) # Date of last completed activity
+    streak_freezes = db.Column(db.Integer, CheckConstraint('streak_freezes <= 5'), default=0)      # Number of freezes owned
+    freeze_used_today = db.Column(db.Boolean, default=False) # used to make sure multiple streak freezes aren't used at once
 
 class Log_reading(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,6 +31,7 @@ class Log_reading(db.Model):
 class Recommend(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    background_info = db.Column(db.JSON, nullable=False)
     common_author = db.Column(db.String(150), nullable=False)
     common_genre = db.Column(db.String(150), nullable=False)
     data = db.Column(db.JSON, nullable=False)
@@ -32,21 +39,30 @@ class Recommend(db.Model):
 class goals(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
-    which_class = db.Column(db.Integer, db.ForeignKey('class_list.id'), nullable=False)
+    #which_class = db.Column(db.Integer, db.ForeignKey('class_list.id'), nullable=False)
     goal_text = db.Column(db.String(300), nullable=False)
     target_minutes = db.Column(db.Integer, nullable=False)
     due_date = db.Column(db.DateTime(timezone=True), nullable=False)
-    class_data = db.relationship('class_list', backref=db.backref('goals', cascade='all, delete-orphan'), lazy=True)
+    #class_data = db.relationship('class_list', backref=db.backref('goals', cascade='all, delete-orphan'), lazy=True)
+
+class currency_logs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer, CheckConstraint('amount <= 500'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
+
+
+"""
 class class_list(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
     class_name = db.Column(db.String(50), nullable=False)
     admin_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     class_code = db.Column(db.String(8), unique=True, nullable=False)
+"""
 
-
+"""
 class class_enrollment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
@@ -54,6 +70,6 @@ class class_enrollment(db.Model):
     student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     classes = db.relationship('class_list', backref=db.backref('enrollments', cascade='all, delete-orphan'), lazy='joined')
     user_data = db.relationship('User', backref='involvement', lazy=True)
-
+"""
 
     
